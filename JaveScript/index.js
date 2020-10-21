9ba5573df5a17667bbef86cd112671ffb7d512ac
Qi <56908202+qi-xmu@users.noreply.github.com>
2020-10-21 14:11:37 +0800

    Initial commit

---
+++ JaveScript/index.js
@@ -0,0 +1,9 @@
+document.write("<strong>Hello World!</strong>");
+var name=document.getElementsByName("h1").values;
+if(name=="this"){
+    alert("Js @welcome！");
+}
+else{
+    console.log(name);
+    alert("ERROR!");
+}
